e4e62198fb8b6b90e75bd2aacf12b26ab93e0368
sghctoma <sghctoma@gmail.com>
2022-09-12 14:30:51 +0200

    [dashboard] Add some error handling and some more comments

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from bokeh.models.widgets.markups import Div
 import msgpack
 import numpy as np
 
@@ -23,6 +24,9 @@ from velocity import update_velocity_band_stats, update_velocity_histogram
 args = curdoc().session_context.request.arguments
 p = Path(args.get('psst')[0].decode('utf-8')).name
 psst_file = Path('data').joinpath(p)
+if not psst_file.exists():
+    curdoc().add_root(Div(text=f"File not found in data directory: {p}"))
+    raise Exception("No such file")
 d = msgpack.unpackb(open(psst_file, 'rb').read())
 
 # XXX for compatibility with older PSST files...
@@ -35,7 +39,7 @@ telemetry = dataclass_from_dict(Telemetry, d)
 try:
     lod = int(args.get('lod')[0])
 except:
-    lod = 100
+    lod = 5
 
 # hst - High Speed Threshold for velocity graphs/statistics in mm/s
 try:
@@ -49,6 +53,7 @@ front_travel, rear_travel = [], []
 front_velocity, rear_velocity = [], []
 front_topouts, rear_topouts = [], []
 front_color, rear_color = Spectral11[1], Spectral11[2]
+front_record_num, rear_record_num, record_num = 0, 0, 0
 
 '''
 Topouts are intervals where suspension is at zero extension for an extended period of time. It allows us to filter
@@ -58,9 +63,10 @@ individually for front and rear suspension.
 '''
 if telemetry.Front.Present:
     front_travel = np.array(telemetry.Front.Travel)
+    front_record_num = len(front_travel)
     front_velocity = np.array(telemetry.Front.Velocity)
     front_topouts = topouts(front_travel, telemetry.Front.Calibration.MaxStroke, telemetry.SampleRate)
-    front_topouts_mask = intervals_mask(front_topouts, len(front_travel))
+    front_topouts_mask = intervals_mask(front_topouts, front_record_num)
 
     if np.count_nonzero(front_topouts_mask):
         p_front_travel_hist = travel_histogram_figure(telemetry.Front.DigitizedTravel, front_travel, front_topouts_mask,
@@ -74,9 +80,10 @@ if telemetry.Front.Present:
 
 if telemetry.Rear.Present:
     rear_travel = np.array(telemetry.Rear.Travel)
+    rear_record_num = len(rear_travel)
     rear_velocity = np.array(telemetry.Rear.Velocity)
     rear_topouts = topouts(rear_travel, telemetry.Frame.MaxRearTravel, telemetry.SampleRate)
-    rear_topouts_mask = intervals_mask(rear_topouts, len(rear_travel))
+    rear_topouts_mask = intervals_mask(rear_topouts, rear_record_num)
 
     if np.count_nonzero(rear_topouts_mask):
         p_rear_travel_hist = travel_histogram_figure(telemetry.Rear.DigitizedTravel, rear_travel, rear_topouts_mask,
@@ -88,11 +95,20 @@ if telemetry.Rear.Present:
     else:
         telemetry.Rear.Present = False
 
-# travel graph event handlers
+if not (front_record_num == 0 or rear_record_num == 0) and front_record_num != rear_record_num:
+    curdoc().add_root(Div(text=f"SST file is corrupt"))
+    raise Exception("Corrupt dataset")
+
+record_num = front_record_num if front_record_num else rear_record_num
+
+'''
+Event handlers for travel graph. We update histograms, statistics and FFTs when a selection is made with the Box Select
+tool, and when the selection is cancelled with a double tap.
+'''
 def on_selectiongeometry(event):
     start = int(event.geometry['x0'] * telemetry.SampleRate)
     end = int(event.geometry['x1'] * telemetry.SampleRate)
-    mask = np.full(len(telemetry.Front.Travel if telemetry.Front.Present else telemetry.Rear.Travel), False)
+    mask = np.full(record_num, False)
     mask[start:end] = True
 
     if telemetry.Front.Present:
@@ -142,18 +158,21 @@ to zero travel, and suspension velocity at the end of the interval reaches a thr
     airtime (usually during drops). I expect this to become a problem if anybody else starts using this program, but
     could not come up with better heuristics so far.
 '''
-comb_topouts = combined_topouts(front_travel if telemetry.Front.Present else np.full(len(rear_travel), 0),
+comb_topouts = combined_topouts(front_travel if telemetry.Front.Present else np.full(record_num, 0),
     telemetry.Front.Calibration.MaxStroke,
-    rear_travel if telemetry.Rear.Present else np.full(len(front_travel), 0),
+    rear_travel if telemetry.Rear.Present else np.full(record_num, 0),
     telemetry.Frame.MaxRearTravel,
     telemetry.SampleRate)
 airtimes = filter_airtimes(comb_topouts,
-    front_velocity if telemetry.Front.Present else np.full(len(rear_velocity), 0),
-    rear_velocity if telemetry.Rear.Present else np.full(len(front_velocity), 0),
+    front_velocity if telemetry.Front.Present else np.full(record_num, 0),
+    rear_velocity if telemetry.Rear.Present else np.full(record_num, 0),
     telemetry.SampleRate)
-airtimes_mask = intervals_mask(np.array(airtimes), len(front_travel if telemetry.Front.Present else rear_travel), False)
+airtimes_mask = intervals_mask(np.array(airtimes), record_num, False)
 add_airtime_labels(airtimes, tick, p_travel)
 
+'''
+Mask out intervals on the travel graph that are ignored in statistics.
+'''
 if telemetry.Front.Present:
     front_idlings = filter_idlings(front_topouts, airtimes_mask)
     add_idling_marks(front_idlings, tick, p_travel)
@@ -162,9 +181,15 @@ if telemetry.Rear.Present:
     rear_idlings = filter_idlings(rear_topouts, airtimes_mask)
     add_idling_marks(rear_idlings, tick, p_travel)
 
+'''
+Leverage-related graphs. These are input data, not measured by this project.
+'''
 p_lr = leverage_ratio_figure(np.array(telemetry.Frame.WheelLeverageRatio), Spectral11[5])
 p_sw = shock_wheel_figure(telemetry.Frame.CoeffsShockWheel, telemetry.Rear.Calibration.MaxStroke, Spectral11[5])
 
+'''
+Construct the layout.
+'''
 travel_hists = column()
 row2 = [travel_hists]
 row3 = []
