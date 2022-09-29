03fbbd11f38f04dc04a0c763052acac2b9c25764
sghctoma <sghctoma@gmail.com>
2022-09-29 19:56:15 +0200

    [gosst] Replace leveldb with sqlite3
    The former does not allow parallel access from separate processes.

--- gosst/gosst.go
+++ gosst/gosst.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"math"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/util"
 	"github.com/ugorji/go/codec"
+
+	_ "modernc.org/sqlite"
 )
 
 type session struct {
@@ -27,31 +28,49 @@ type calibrationPair struct {
 }
 
 type RequestHandler struct {
-    Db *leveldb.DB
+    Db *sql.DB
     H codec.Handle
 }
 
 func (this *RequestHandler) GetCalibrations(c *gin.Context) {
-    m := make(map[string]calibrationPair)
-    iter := this.Db.NewIterator(util.BytesPrefix([]byte("cal-")), nil)
-    for iter.Next() {
-        dec := codec.NewDecoderBytes(iter.Value(), this.H)
+    m := make(map[int64]calibrationPair)
+    rows, err := this.Db.Query("SELECT ROWID, data FROM calibrations")
+	if err != nil {
+	    c.AbortWithStatus(http.StatusInternalServerError)
+        return
+	}
+    for rows.Next() {
+        var id int64
+        var data []byte
+        rows.Scan(&id, &data)
+
+        dec := codec.NewDecoderBytes(data, this.H)
         var cal calibrationPair
         dec.Decode(&cal)
-        m[string(iter.Key()[:])] = cal
+        m[id] = cal
     }
-    iter.Release()
     c.JSON(http.StatusOK, m)
 }
 
 func (this *RequestHandler) GetCalibration(c *gin.Context) {
-    b, err := this.Db.Get([]byte("cal-" + c.Param("id")), nil)
+    id, err := strconv.ParseInt(c.Param("id"), 10, 64)
     if err != nil {
-        c.AbortWithStatus(http.StatusNotFound)
+        c.AbortWithStatus(http.StatusBadRequest)
         return
     }
 
-    dec := codec.NewDecoderBytes(b, this.H)
+    var data []byte
+    err = this.Db.QueryRow("SELECT data FROM calibrations where ROWID = ?", id).Scan(&data)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            c.AbortWithStatus(http.StatusNotFound)
+        } else {
+            c.AbortWithStatus(http.StatusInternalServerError)
+        }
+        return
+    }
+
+    dec := codec.NewDecoderBytes(data, this.H)
     var cal calibrationPair
     dec.Decode(&cal)
     c.JSON(http.StatusOK, cal)
@@ -72,20 +91,25 @@ func (this *RequestHandler) PutCalibration(c *gin.Context) {
     }
     calibration.Front.StartAngle = math.Acos(calibration.Front.MaxDistance / 2.0 / calibration.Front.ArmLength)
     calibration.Rear.StartAngle = math.Acos(calibration.Rear.MaxDistance / 2.0 / calibration.Rear.ArmLength)
-    var b []byte
-    enc := codec.NewEncoderBytes(&b, this.H)
+    var data []byte
+    enc := codec.NewEncoderBytes(&data, this.H)
     enc.Encode(calibration)
 
-    id := uuid.NewString()
-    if err := this.Db.Put([]byte("cal-" + id), b, nil); err != nil {
+    if _, err := this.Db.Exec("INSERT INTO calibrations VALUES (?)", data); err != nil {
         c.AbortWithStatus(http.StatusInternalServerError)
     } else {
-        c.JSON(http.StatusCreated, gin.H{"id": id})
+        c.Status(http.StatusCreated)
     }
 }
 
 func (this *RequestHandler) DeleteCalibration (c *gin.Context) {
-    if err := this.Db.Delete([]byte("cal-" + c.Param("id")), nil); err != nil {
+    id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+    if err != nil {
+        c.AbortWithStatus(http.StatusBadRequest)
+        return
+    }
+
+    if _, err := this.Db.Exec("DELETE FROM calibrations WHERE ROWID = ?", id); err != nil {
         c.AbortWithStatus(http.StatusInternalServerError)
     } else {
         c.Status(http.StatusNoContent)
@@ -93,26 +117,44 @@ func (this *RequestHandler) DeleteCalibration (c *gin.Context) {
 }
 
 func (this *RequestHandler) GetLinkages(c *gin.Context) {
-    m := make(map[string]linkage)
-    iter := this.Db.NewIterator(util.BytesPrefix([]byte("lnk-")), nil)
-    for iter.Next() {
-        dec := codec.NewDecoderBytes(iter.Value(), this.H)
+    m := make(map[int64]linkage)
+    rows, err := this.Db.Query("SELECT ROWID, data FROM linkages")
+	if err != nil {
+	    c.AbortWithStatus(http.StatusInternalServerError)
+        return
+	}
+    for rows.Next() {
+        var id int64
+        var data []byte
+        rows.Scan(&id, &data)
+
+        dec := codec.NewDecoderBytes(data, this.H)
         var linkage linkage
         dec.Decode(&linkage)
-        m[string(iter.Key()[:])] = linkage
+        m[id] = linkage
     }
-    iter.Release()
     c.JSON(http.StatusOK, m)
 }
 
 func (this *RequestHandler) GetLinkage(c *gin.Context) {
-    b, err := this.Db.Get([]byte("lnk-" + c.Param("id")), nil)
+    id, err := strconv.ParseInt(c.Param("id"), 10, 64)
     if err != nil {
-        c.AbortWithStatus(http.StatusNotFound)
+        c.AbortWithStatus(http.StatusBadRequest)
         return
     }
 
-    dec := codec.NewDecoderBytes(b, this.H)
+    var data []byte
+    err = this.Db.QueryRow("SELECT data FROM linkages where ROWID = ?", id).Scan(&data)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            c.AbortWithStatus(http.StatusNotFound)
+        } else {
+            c.AbortWithStatus(http.StatusInternalServerError)
+        }
+        return
+    }
+
+    dec := codec.NewDecoderBytes(data, this.H)
     var linkage linkage
     dec.Decode(&linkage)
     c.JSON(http.StatusOK, linkage)
@@ -123,20 +165,25 @@ func (this *RequestHandler) PutLinkage(c *gin.Context) {
     file, _ := c.FormFile("leverage")
     f, _ := file.Open()
     linkage := newLinkage(name, f)
-    var b []byte
-    enc := codec.NewEncoderBytes(&b, this.H)
+    var data []byte
+    enc := codec.NewEncoderBytes(&data, this.H)
     enc.Encode(linkage)
 
-    id := uuid.NewString()
-    if err := this.Db.Put([]byte("lnk-" + id), b, nil); err != nil {
+    if _, err := this.Db.Exec("INSERT INTO linkages VALUES (?)", data); err != nil {
         c.AbortWithStatus(http.StatusInternalServerError)
     } else {
-        c.JSON(http.StatusCreated, gin.H{"id": id})
+        c.Status(http.StatusCreated)
     }
 }
 
 func (this *RequestHandler) DeleteLinkage(c *gin.Context) {
-    if err := this.Db.Delete([]byte("lnk-" + c.Param("id")), nil); err != nil {
+    id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+    if err != nil {
+        c.AbortWithStatus(http.StatusBadRequest)
+        return
+    }
+
+    if _, err := this.Db.Exec("DELETE FROM linkages WHERE ROWID = ?", id); err != nil {
         c.AbortWithStatus(http.StatusInternalServerError)
     } else {
         c.Status(http.StatusNoContent)
@@ -144,66 +191,110 @@ func (this *RequestHandler) DeleteLinkage(c *gin.Context) {
 }
 
 func (this *RequestHandler) GetSessions(c *gin.Context) {
-    m := make(map[string]session)
-    iter := this.Db.NewIterator(util.BytesPrefix([]byte("ses-")), nil)
-    for iter.Next() {
-        dec := codec.NewDecoderBytes(iter.Value(), this.H)
-        var session session
-        dec.Decode(&session)
-        m[string(iter.Key()[:])] = session
+    m := make(map[int64]gin.H)
+    rows, err := this.Db.Query("SELECT ROWID, name, description, date FROM sessions")
+	if err != nil {
+	    c.AbortWithStatus(http.StatusInternalServerError)
+        return
+	}
+    for rows.Next() {
+        var id int64
+        var name, description string
+        var date int64
+        rows.Scan(&id, &name, &description, &date)
+
+        m[id] = gin.H{"id": id, "name": name, "description": description, "date": time.Unix(date, 0)}
     }
-    iter.Release()
     c.JSON(http.StatusOK, m)
 }
 
 func (this *RequestHandler) GetSession(c *gin.Context) {
-    b, err := this.Db.Get([]byte("ses-" + c.Param("id")), nil)
+    id, err := strconv.ParseInt(c.Param("id"), 10, 64)
     if err != nil {
-        c.AbortWithStatus(http.StatusNotFound)
+        c.AbortWithStatus(http.StatusBadRequest)
         return
     }
 
-    dec := codec.NewDecoderBytes(b, this.H)
-    var session session
-    dec.Decode(&session)
-    c.JSON(http.StatusOK, session)
+    var name, description string
+    var date int64
+    err = this.Db.QueryRow("SELECT name, description, date FROM sessions where ROWID = ?", id).Scan(&name, &description, &date)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            c.AbortWithStatus(http.StatusNotFound)
+        } else {
+            log.Println(err)
+            c.AbortWithStatus(http.StatusInternalServerError)
+        }
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"name": name, "description": description, "date": time.Unix(date, 0)})
 }
 
 func (this *RequestHandler) GetSessionData(c *gin.Context) {
-    bs, err := this.Db.Get([]byte("ses-" + c.Param("id")), nil)
+    id, err := strconv.ParseInt(c.Param("id"), 10, 64)
     if err != nil {
-        c.AbortWithStatus(http.StatusNotFound)
+        c.AbortWithStatus(http.StatusBadRequest)
         return
     }
 
-    dec := codec.NewDecoderBytes(bs, this.H)
-    var session session
-    dec.Decode(&session)
+    var name string
+    var data []byte
+    err = this.Db.QueryRow("SELECT name, data FROM sessions where ROWID = ?", id).Scan(&name, &data)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            c.AbortWithStatus(http.StatusNotFound)
+        } else {
+            c.AbortWithStatus(http.StatusInternalServerError)
+        }
+        return
+    }
 
-    var bd []byte
-    enc := codec.NewEncoderBytes(&bd, this.H)
-    enc.Encode(session.Data)
-    c.Header("Content-Disposition", "attachment; filename=" + session.Name)
-    c.Data(http.StatusOK, "application/octet-stream", bd)
+    c.Header("Content-Disposition", "attachment; filename=" + name)
+    c.Data(http.StatusOK, "application/octet-stream", data)
 }
 
 func (this *RequestHandler) PutSession(c *gin.Context) {
-    var cpair calibrationPair
-    bc, err := this.Db.Get([]byte("cal-" + c.PostForm("calibration")), nil)
+    cid, err := strconv.ParseInt(c.PostForm("calibration"), 10, 64)
     if err != nil {
-        c.AbortWithStatus(http.StatusNotFound)
+        c.AbortWithStatus(http.StatusBadRequest)
         return
     }
-    cdec := codec.NewDecoderBytes(bc, this.H)
+
+    var calData []byte
+    err = this.Db.QueryRow("SELECT data FROM calibrations where ROWID = ?", cid).Scan(&calData)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            c.AbortWithStatus(http.StatusNotFound)
+        } else {
+            c.AbortWithStatus(http.StatusInternalServerError)
+        }
+        return
+    }
+
+    cdec := codec.NewDecoderBytes(calData, this.H)
+    var cpair calibrationPair
     cdec.Decode(&cpair)
 
-    var linkage linkage
-    bl, err := this.Db.Get([]byte("lnk-" + c.PostForm("linkage")), nil)
+    lid, err := strconv.ParseInt(c.PostForm("linkage"), 10, 64)
     if err != nil {
-        c.AbortWithStatus(http.StatusNotFound)
+        c.AbortWithStatus(http.StatusBadRequest)
         return
     }
-    ldec := codec.NewDecoderBytes(bl, this.H)
+
+    var lnkData []byte
+    err = this.Db.QueryRow("SELECT data FROM linkages where ROWID = ?", lid).Scan(&lnkData)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            c.AbortWithStatus(http.StatusNotFound)
+        } else {
+            c.AbortWithStatus(http.StatusInternalServerError)
+        }
+        return
+    }
+
+    ldec := codec.NewDecoderBytes(lnkData, this.H)
+    var linkage linkage
     ldec.Decode(&linkage)
 
     file, _ := c.FormFile("recording")
@@ -213,25 +304,26 @@ func (this *RequestHandler) PutSession(c *gin.Context) {
         return
     }
 
-    var bs []byte
-    enc := codec.NewEncoderBytes(&bs, this.H)
-    enc.Encode(&session{
-        Date: time.Now(),
-        Name: pd.Name,
-        Description: c.PostForm("description"),
-        Data: *pd,
-    })
+    var data []byte
+    enc := codec.NewEncoderBytes(&data, this.H)
+    enc.Encode(pd)
 
-    id := uuid.NewString()
-    if err := this.Db.Put([]byte("ses-" + id), bs, nil); err != nil {
+    if _, err := this.Db.Exec("INSERT INTO sessions VALUES (?, ?, ?, ?)",
+            pd.Name, c.PostForm("description"), time.Now().Unix(), data); err != nil {
         c.AbortWithStatus(http.StatusInternalServerError)
     } else {
-        c.JSON(http.StatusCreated, gin.H{"id": id})
+        c.Status(http.StatusCreated)
     }
 }
 
 func (this *RequestHandler) DeleteSession(c *gin.Context) {
-    if err := this.Db.Delete([]byte("ses-" + c.Param("id")), nil); err != nil {
+    id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+    if err != nil {
+        c.AbortWithStatus(http.StatusBadRequest)
+        return
+    }
+
+    if _, err := this.Db.Exec("DELETE FROM sessions WHERE ROWID = ?", id); err != nil {
         c.AbortWithStatus(http.StatusInternalServerError)
     } else {
         c.Status(http.StatusNoContent)
@@ -241,11 +333,16 @@ func (this *RequestHandler) DeleteSession(c *gin.Context) {
 func main() {
     var h codec.MsgpackHandle
 
-    db, err := leveldb.OpenFile("data/gosst.db", nil)
+    db, err := sql.Open("sqlite", "data/gosst.db")
     if err != nil {
         log.Fatal("could not open database")
     }
-    defer db.Close()
+    if _, err := db.Exec(`
+            CREATE TABLE IF NOT EXISTS calibrations(data BLOB);
+            CREATE TABLE IF NOT EXISTS linkages(data BLOB);
+            CREATE TABLE IF NOT EXISTS sessions(name TEXT, description TEXT, date INTEGER, data BLOB);`); err != nil {
+        log.Fatal("could not create data tables")
+    }
 
     //XXX gin.SetMode(gin.ReleaseMode)
     router := gin.Default()
