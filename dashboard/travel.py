156916f7d9d497aab7c28b8531dbe86bd18eaede
sghctoma <sghctoma@gmail.com>
2023-03-01 13:33:38 +0100

    [ui] Add a bit of a gap between travel hist. bars

--- dashboard/travel.py
+++ dashboard/travel.py
@@ -160,7 +160,7 @@ def travel_histogram_figure(digitized, travel, mask, color, title):
     p.x_range.start = 0
     p.x_range.end = HISTOGRAM_RANGE_MULTIPLIER * np.max(data['right'])
     p.y_range.flipped = True
-    p.hbar(y='y', height=max_travel / (len(bins) - 1) - 2, left=0, right='right',
+    p.hbar(y='y', height=max_travel / (len(bins) - 1) - 3, left=0, right='right',
            source=source, line_width=2, color=color, fill_alpha=0.4)
 
     _add_travel_stat_labels(travel[mask], max_travel, p.x_range.end, p)
