5428fd0b9955233c0e9c82dd373f2d60249e9a34
sghctoma <sghctoma@gmail.com>
2023-04-02 06:32:34 +0200

    [ui] Rename db primary keys to 'id'

--- dashboard/database.py
+++ dashboard/database.py
@@ -17,7 +17,7 @@ tokens_table = Table(
 bokeh_components_table = Table(
     'bokeh_components',
     metadata_obj,
-    Column('session_id', Integer, ForeignKey('sessions.session_id')),
+    Column('session_id', Integer, ForeignKey('sessions.id')),
     Column('script', String, nullable=False),
     Column('travel', String, nullable=False),
     Column('velocity', String, nullable=False),
@@ -39,39 +39,39 @@ bokeh_components_table = Table(
 tracks_table = Table(
     'tracks',
     metadata_obj,
-    Column('track_id', Integer, primary_key=True),
+    Column('id', Integer, primary_key=True),
     Column('track', String, nullable=False),
 )
 
 sessions_table = Table(
     'sessions',
     metadata_obj,
-    Column('session_id', Integer, primary_key=True),
+    Column('id', Integer, primary_key=True),
     Column('name', String),
-    Column('setup_id', Integer, ForeignKey('setups.setup_id'), nullable=False),
+    Column('setup_id', Integer, ForeignKey('setups.id'), nullable=False),
     Column('description', String),
     Column('timestamp', Integer, nullable=False),
     Column('data', LargeBinary, nullable=False),
-    Column('track_id', Integer, ForeignKey('tracks.track_id')),
+    Column('track_id', Integer, ForeignKey('tracks.id')),
 )
 
 setups_table = Table(
     'setups',
     metadata_obj,
-    Column('setup_id', Integer, primary_key=True),
+    Column('id', Integer, primary_key=True),
     Column('name', String, nullable=False),
-    Column('linkage_id', Integer, ForeignKey('linkages.linkage_id'),
+    Column('linkage_id', Integer, ForeignKey('linkages.id'),
            nullable=False),
     Column('front_calibration_id', Integer,
-           ForeignKey('calibrations.calibration_id')),
+           ForeignKey('calibrations.id')),
     Column('rear_calibration_id', Integer,
-           ForeignKey('calibrations.calibration_id')),
+           ForeignKey('calibrations.id')),
 )
 
 calibrations_table = Table(
     'calibrations',
     metadata_obj,
-    Column('calibration_id', Integer, primary_key=True),
+    Column('id', Integer, primary_key=True),
     Column('name', String, nullable=False),
     Column('arm', Float, nullable=False),
     Column('dist', Float, nullable=False),
@@ -82,7 +82,7 @@ calibrations_table = Table(
 linkages_table = Table(
     'linkages',
     metadata_obj,
-    Column('linkage_id', Integer, primary_key=True),
+    Column('id', Integer, primary_key=True),
     Column('name', String, nullable=False),
     Column('raw_lr_data', String, nullable=False),
 )
@@ -94,11 +94,12 @@ def stmt_tokens():
 
 def stmt_sessions():
     return (select(
-        sessions_table.c.session_id,
+        sessions_table.c.id,
         sessions_table.c.name,
         sessions_table.c.description,
         sessions_table.c.timestamp)
-        .join(bokeh_components_table, sessions_table.c.session_id == bokeh_components_table.c.session_id)
+        .join(bokeh_components_table,
+              sessions_table.c.id == bokeh_components_table.c.session_id)
         .order_by(desc(sessions_table.c.timestamp)))
 
 
@@ -109,13 +110,13 @@ def stmt_session(session_id: int):
         sessions_table.c.data,
         tracks_table.c.track)
         .join(tracks_table, isouter=True)
-        .where(sessions_table.c.session_id == session_id))
+        .where(sessions_table.c.id == session_id))
 
 
 def stmt_session_delete(session_id: int):
     return (delete(
         sessions_table)
-        .where(sessions_table.c.session_id == session_id))
+        .where(sessions_table.c.id == session_id))
 
 
 def stmt_cache(session_id: int):
@@ -137,7 +138,7 @@ def stmt_cache_delete(session_id: int):
 def stmt_track(track: str):
     return (insert(tracks_table)
             .values(track=track)
-            .returning(tracks_table.c.track_id))
+            .returning(tracks_table.c.id))
 
 
 def stmt_session_tracks(session_id: int, track_id: int,
@@ -145,14 +146,14 @@ def stmt_session_tracks(session_id: int, track_id: int,
     s1 = sessions_table.alias()
     s2 = sessions_table.alias()
     stmt_select = (select(
-        s1.c.session_id)
-        .join(s2, s1.c.setup_id == s2.c.setup_id)
-        .where(s1.c.session_id == session_id)
+        s1.c.id)
+        .join(s2, s1.c.id == s2.c.id)
+        .where(s1.c.id == session_id)
         .where(s2.c.timestamp >= start_time)
         .where(s2.c.timestamp <= end_time))
     stmt_update = (update(
         sessions_table)
-        .where(sessions_table.c.session_id.in_(stmt_select))
+        .where(sessions_table.c.id.in_(stmt_select))
         .values(track_id=track_id))
     return stmt_update
 
@@ -160,7 +161,7 @@ def stmt_session_tracks(session_id: int, track_id: int,
 def stmt_description(session_id: int, name: str, desc: str):
     return (update(
         sessions_table)
-        .where(sessions_table.c.session_id == session_id)
+        .where(sessions_table.c.id == session_id)
         .values(name=name, description=desc))
 
 
@@ -172,8 +173,8 @@ def stmt_setup(session_id: int):
         linkages_table.c.name,
         fcal,
         rcal)
-        .join(linkages_table, linkages_table.c.linkage_id == setups_table.c.linkage_id)
-        .join(fcal, fcal.c.calibration_id == setups_table.c.front_calibration_id)
-        .join(rcal, rcal.c.calibration_id == setups_table.c.rear_calibration_id)
-        .join(sessions_table, sessions_table.c.setup_id == setups_table.c.setup_id)
-        .where(sessions_table.c.session_id == session_id))
+        .join(linkages_table, linkages_table.c.id == setups_table.c.linkage_id)
+        .join(fcal, fcal.c.id == setups_table.c.front_calibration_id)
+        .join(rcal, rcal.c.id == setups_table.c.rear_calibration_id)
+        .join(sessions_table, sessions_table.c.id == setups_table.c.id)
+        .where(sessions_table.c.id == session_id))
