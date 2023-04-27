d1f87a73a7b4b5cf29435d623c093c2d00afdd07
sghctoma <sghctoma@gmail.com>
2023-04-27 10:36:39 +0200

    [api] Add API to create setup in one go

--- dashboard/app/api/setup/routes.py
+++ dashboard/app/api/setup/routes.py
@@ -5,8 +5,11 @@ from flask_jwt_extended import jwt_required
 
 from app.api.setup import bp
 from app.extensions import db
+from app.models.board import Board
+from app.models.calibration import Calibration
+from app.models.linkage import Linkage
 from app.models.setup import Setup
-from app.telemetry.psst import dataclass_from_dict
+from app.telemetry.psst import dataclass_from_dict as dfd
 
 
 @bp.route('', methods=['GET'])
@@ -35,9 +38,66 @@ def delete(id: int):
 @bp.route('', methods=['PUT'])
 @jwt_required()
 def put():
-    entity = dataclass_from_dict(Setup, request.json)
+    entity = dfd(Setup, request.json)
     if not entity:
         return jsonify(msg="Invalid data for Setup"), status.BAD_REQUEST
     entity = db.session.merge(entity)
     db.session.commit()
     return jsonify(id=entity.id), status.CREATED
+
+
+@bp.route('/combined', methods=['PUT'])
+@jwt_required()
+def put_combined():
+    linkage = dfd(Linkage, request.json['linkage'])
+    if not linkage or not linkage.validate():
+        return jsonify(msg="Invalid data for linkage!"), status.BAD_REQUEST
+
+    front_calibration = (dfd(Calibration, request.json['front_calibration'])
+                         if 'front_calibration' in request.json else None)
+    if front_calibration and not front_calibration.validate():
+        return jsonify(msg="Invalid data for calibration!"), status.BAD_REQUEST
+
+    rear_calibration = (dfd(Calibration, request.json['rear_calibration'])
+                        if 'rear_calibration' in request.json else None)
+    if rear_calibration and not rear_calibration.validate():
+        return jsonify(msg="Invalid data for calibration!"), status.BAD_REQUEST
+
+    board = (dfd(Board, request.json['board'])
+             if 'board' in request.json else None)
+
+    if not front_calibration and not rear_calibration:
+        return jsonify(msg="No calibration given"), status.BAD_REQUEST
+
+    if not linkage:
+        return jsonify(msg="Missing or invalid linkage"), status.BAD_REQUEST
+
+    setup = Setup(
+        name=request.json['name'],
+    )
+    try:
+        with db.session.begin_nested():
+            linkage = db.session.merge(linkage)
+            db.session.flush()
+            setup.linkage_id = linkage.id
+            if front_calibration:
+                front_calibration = db.session.merge(front_calibration)
+                db.session.flush()
+                setup.front_calibration_id = front_calibration.id
+            if rear_calibration:
+                rear_calibration = db.session.merge(rear_calibration)
+                db.session.flush()
+                setup.rear_calibration_id = rear_calibration.id
+            setup = db.session.merge(setup)
+            db.session.flush()
+            if board:
+                board.setup_id = setup.id
+                db.session.merge(board)
+                db.session.flush()
+            db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return (jsonify(msg=f"Transaction failed: {e}"),
+                status.INTERNAL_SERVER_ERROR)
+
+    return jsonify(id=setup.id), status.CREATED
