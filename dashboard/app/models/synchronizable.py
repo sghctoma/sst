4b86a16c61099498869aafaceb51b790e1bbe805
sghctoma <sghctoma@gmail.com>
2024-02-06 08:35:42 +0100

    [db] Set sync-related timestamps automatically

--- dashboard/app/models/synchronizable.py
+++ dashboard/app/models/synchronizable.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 from app.extensions import db
 
 
 class Synchronizable(object):
-    updated = db.Column(db.Integer, server_default=db.func.unixepoch())
-    client_updated = db.Column(db.Integer)
+    updated = db.Column(db.Integer, server_default=db.func.unixepoch(),
+                        onupdate=lambda: int(datetime.now().timestamp()))
+    client_updated = db.Column(db.Integer, server_default=db.text('0'))
     deleted = db.Column(db.Integer)
