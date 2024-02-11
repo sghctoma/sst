cf6094804225f5f3ad247c0c3a5b950d3f9a0e05
sghctoma <sghctoma@gmail.com>
2024-02-11 10:51:22 +0100

    [test] Add tests for /api/session endpoint

---
+++ dashboard/tests/api/test_session.py
@@ -0,0 +1,251 @@
+import base64
+import hashlib
+import uuid
+
+import pytest
+
+from flask import current_app
+from http import HTTPStatus as status
+
+from conftest import DB_IDS, session_data, track_gpx
+
+
+def test_get_all(client):
+    response = client.get('/api/session')
+    ids = [c['id'] for c in response.json]
+    assert str(DB_IDS['session']) in ids
+
+
+def test_get_psst(client):
+    id = str(DB_IDS['session'])
+    response = client.get(f'/api/session/{id}/psst')
+    session_data_hash = hashlib.sha256(session_data).digest().hex()
+    assert hashlib.sha256(response.data).digest().hex() == session_data_hash
+
+
+def test_get_psst_nonexistent(client):
+    response = client.get(f'/api/session/{DB_IDS["nonexistent"]}/psst')
+    assert response.status_code == status.NOT_FOUND
+
+
+def test_get_last(client):
+    id = str(DB_IDS['session_html'])
+    response = client.get('/api/session/last')
+    assert id == response.json['id']
+
+
+def test_get_psst_invalid_uuid(client):
+    with pytest.raises(ValueError) as e:
+        client.get('/api/session/xxxx/psst')
+    assert "badly formed" in str(e.value)
+
+
+def test_get(client):
+    id = str(DB_IDS['session'])
+    response = client.get(f'/api/session/{id}')
+    assert id == response.json['id']
+
+
+def test_get_invalid_uuid(client):
+    with pytest.raises(ValueError) as e:
+        client.get('/api/session/xxxx')
+    assert "badly formed" in str(e.value)
+
+
+def test_get_nonexistent(client):
+    response = client.get(f'/api/session/{DB_IDS["nonexistent"]}')
+    assert response.status_code == status.NOT_FOUND
+
+
+def test_filter(client):
+    id = str(DB_IDS['session'])
+    start = 10
+    end = 13
+    response = client.get(
+        f'/api/session/{id}/filter?start={start}&end={end}')
+    assert (hashlib.sha256(response.data).digest().hex() ==
+            'e68e4aec59b75367578bd7045e43d2914f0b5152b7a09794ee4f87de08d16124')
+
+
+@pytest.mark.parametrize(
+    ('start', 'end'),
+    (
+        (-13, 13),
+        (0, 1337),
+        (13, 0),
+    )
+)
+def test_filter_invalid_input(client, start, end):
+    id = str(DB_IDS['session'])
+    response = client.get(
+        f'/api/session/{id}/filter?start={start}&end={end}')
+    assert (hashlib.sha256(response.data).digest().hex() ==
+            '83a1412a9a8c0f7b97b15121c6c33eedca0e89babe2312d65739d79c7c23d9c7')
+
+
+def test_delete(client, auth):
+    auth.login()
+
+    client.delete(f'/api/session/{DB_IDS["session_html"]}')
+    response = client.get(f'/api/session/{DB_IDS["session_html"]}')
+    assert response.status_code == status.NOT_FOUND
+
+
+def test_put(app, client, auth, requests_mock):
+    auth.login()
+
+    with app.app_context():
+        api_server = current_app.config['GOSST_HTTP_API']
+    url = f'{api_server}/api/internal/session'
+    id = str(uuid.uuid4())
+    requests_mock.put(url, json={'id': id}, status_code=status.CREATED)
+
+    response = client.put('/api/session', json={"id": "dummy"})
+    assert response.status_code == status.CREATED
+    assert response.json['id'] == id
+
+
+def test_put_invalid_data(app, client, auth, requests_mock):
+    auth.login()
+
+    with app.app_context():
+        api_server = current_app.config['GOSST_HTTP_API']
+    url = f'{api_server}/api/internal/session'
+    id = str(uuid.uuid4())
+    requests_mock.put(url, json={'id': id}, status_code=status.BAD_REQUEST)
+
+    response = client.put('/api/session', json={"id": "dummy"})
+    assert response.status_code == status.BAD_REQUEST
+
+
+def test_put_normalized(app, client, auth, requests_mock):
+    auth.login()
+
+    with app.app_context():
+        api_server = current_app.config['GOSST_HTTP_API']
+    url = f'{api_server}/api/internal/session/normalized'
+    id = str(uuid.uuid4())
+    requests_mock.put(url, json={'id': id}, status_code=status.CREATED)
+
+    response = client.put('/api/session/normalized', json={"id": "dummy"})
+    assert response.status_code == status.CREATED
+    assert response.json['id'] == id
+
+
+def test_put_normalized_invalid_data(app, client, auth, requests_mock):
+    auth.login()
+
+    with app.app_context():
+        api_server = current_app.config['GOSST_HTTP_API']
+    url = f'{api_server}/api/internal/session'
+    id = str(uuid.uuid4())
+    requests_mock.put(url, json={'id': id}, status_code=status.BAD_REQUEST)
+
+    response = client.put('/api/session', json={"id": "dummy"})
+    assert response.status_code == status.BAD_REQUEST
+
+
+def test_put_processed(client, auth):
+    auth.login()
+
+    id = str(uuid.uuid4())
+    s_json = dict(
+        id=id,
+        name="test_session",
+        description="session description",
+        setup=DB_IDS['setup'],
+        track=DB_IDS['track'],
+        timestamp=1683457678,
+        data=base64.b64encode(session_data).decode('utf-8'),
+    )
+    response = client.put('/api/session/psst', json=s_json)
+    assert response.status_code == status.CREATED
+    assert response.json['id'] == id
+
+
+def test_put_processed_invalid_data(client, auth):
+    auth.login()
+
+    id = str(uuid.uuid4())
+    s_json = dict(
+        id=id,
+        name="test_session",
+        description="session description",
+        setup=DB_IDS['setup'],
+        track=DB_IDS['track'],
+        timestamp=1683457678,
+        data=base64.b64encode(b'test').decode('utf-8'),
+    )
+    response = client.put('/api/session/psst', json=s_json)
+    assert response.status_code == status.BAD_REQUEST
+
+
+def test_patch(auth, client):
+    auth.login()
+
+    patch_json = {'name': 'new_name', 'desc': 'new_description'}
+    client.patch(f'/api/session/{DB_IDS["session"]}', json=patch_json)
+    response = client.get(f'/api/session/{DB_IDS["session"]}')
+    assert response.json['name'] == 'new_name'
+    assert response.json['description'] == 'new_description'
+
+
+@pytest.mark.parametrize(
+    ('id', 'message'),
+    (
+        (DB_IDS['session'], None),
+        (DB_IDS['nonexistent'], b'does not exist'),
+        (DB_IDS['session_html'], b'already generated'),
+    )
+)
+def test_generate_bokeh(client, id, message):
+    response = client.put(f'/api/session/{id}/bokeh')
+    assert (response.status_code == status.NO_CONTENT or
+            message in response.data)
+
+
+@pytest.mark.parametrize(
+    ('id', 'status'),
+    (
+        (DB_IDS['nonexistent'], status.NOT_FOUND),
+        (DB_IDS['session'], status.NOT_FOUND),
+        (DB_IDS['session_html'], status.OK),
+    )
+)
+def test_session_html(client, auth, id, status):
+    auth.login()
+
+    response = client.get(f'/api/session/{id}/bokeh')
+    assert response.status_code == status
+
+
+def test_session_html_last(client, auth):
+    auth.login()
+
+    response = client.get('/api/session/last/bokeh')
+    assert response.status_code == status.OK
+    assert response.json['id'] == str(DB_IDS['session_html'])
+
+
+def test_upload_gpx(client, auth):
+    auth.login()
+
+    id = DB_IDS['session_html']
+    response = client.put(f'/api/session/{id}/gpx', data=track_gpx)
+    assert response.status_code == status.OK
+    assert hashlib.sha256(response.data).digest().hex() == (
+        'acc6781678324e4b6dc52b8c7e76634f5d42276afb79612380d519f92aa26c04')
+
+
+@pytest.mark.parametrize(
+    ('id', 'status'),
+    (
+        (DB_IDS['nonexistent'], status.NOT_FOUND),
+        (DB_IDS['session'], status.BAD_REQUEST),
+    )
+)
+def test_upload_gpx_input_validation(client, auth, id, status):
+    auth.login()
+
+    response = client.put(f'/api/session/{id}/gpx', data=track_gpx)
+    assert response.status_code == status
--- dashboard/tests/conftest.py
+++ dashboard/tests/conftest.py
@@ -13,6 +13,7 @@ from app.models.board import Board
 from app.models.calibration import Calibration
 from app.models.linkage import Linkage
 from app.models.session import Session
+from app.models.session_html import SessionHtml
 from app.models.setup import Setup
 from app.models.track import Track
 from app.models.user import User
@@ -26,6 +27,8 @@ DB_IDS = dict(
     setup=uuid.UUID('7d1cc6ea25eb47f9a83d25b5e0a0179f'),
     track=uuid.UUID('3fb905fd802740b4a13d43a81f36d81d'),
     session=uuid.UUID('d85d5df4562c4b878eebaeb7bb676ec9'),
+    session2=uuid.UUID('b8789424e29c4d5fa288c5b614adea3d'),
+    session_html=uuid.UUID('b8789424e29c4d5fa288c5b614adea3d'),
     board='0011223344556677',
     calibration_method_fraction=uuid.UUID('230e04a092ce42189a3c23bf3cde2b05'),
     calibration_method_percentage=uuid.UUID('c619045af435427797cb1e2c1fddcfeb'),
@@ -41,9 +44,14 @@ with open(os.path.join(os.path.dirname(__file__), 'linkage.txt'), 'rb') as f:
 with open(os.path.join(os.path.dirname(__file__), 'track.json'), 'rb') as f:
     track_data = f.read().decode('utf8')
 
+with open(os.path.join(os.path.dirname(__file__), 'track.gpx'), 'rb') as f:
+    track_gpx = f.read().decode('utf8')
 
 with open(os.path.join(os.path.dirname(__file__), 'test.psst'), 'rb') as f:
     session_data = f.read()
+
+with open(os.path.join(os.path.dirname(__file__), 'session.js'), 'rb') as f:
+    session_script = f.read().decode('utf8')
 
 
 def add_test_data():
@@ -84,10 +92,39 @@ def add_test_data():
         description="session description",
         setup=DB_IDS['setup'],
         track=DB_IDS['track'],
-        timestamp=1683457678,
+        timestamp=1500000000,
     )
     session.data = session_data
     board = Board(id=DB_IDS['board'], setup_id=DB_IDS['setup'])
+
+    # A second session is only needed so that we also have a session with an
+    # already generated session_html.
+    session2 = Session(
+        id=DB_IDS['session_html'],
+        name="test_session",
+        description="session description",
+        setup=DB_IDS['setup'],
+        track=DB_IDS['track'],
+        timestamp=1683457678,
+    )
+    session2.data = session_data
+    session_html = SessionHtml(
+        session_id=DB_IDS['session_html'],
+        script=session_script,
+        travel='<div id="a13dfdbf-19b4-41de-98f1-4251281bd1f9" data-root-id="p1968" style="display: contents;"></div>',
+        velocity='<div id="b942c6c9-400a-4dcd-bf26-fb759997141f" data-root-id="p2089" style="display: contents;"></div>',
+        map='<div id="ef4c2de6-b611-4d9f-bc51-3b8237ecf610" data-root-id="p2349" style="display: contents;"></div>',
+        lr='<div id="c390f586-2dde-42bf-bb70-f167236d59d1" data-root-id="p2141" style="display: contents;"></div>',
+        sw='<div id="a0bb159f-829a-4d92-a8a6-ad744800f666" data-root-id="p2173" style="display: contents;"></div>',
+        f_thist='<div id="fc86b57d-f4c6-42ea-9776-89e192e5df8a" data-root-id="p1004" style="display: contents;"></div>',
+        f_fft='<div id="ec36612a-d13c-4e96-a707-079b33721215" data-root-id="p1444" style="display: contents;"></div>',
+        f_vhist='<div id="cd1feb3d-d6d3-4b77-8811-a929089db21d" data-root-id="p2408" style="display: contents;"></div>',
+        r_thist='<div id="e7c214ef-cdbf-41a2-88fa-d5ab2219c16b" data-root-id="p1486" style="display: contents;"></div>',
+        r_fft='<div id="c0478ff9-1143-49ab-b783-3e5426cb585e" data-root-id="p1926" style="display: contents;"></div>',
+        r_vhist='<div id="ec8fa845-ccc0-4aa0-871e-eb6b173786ba" data-root-id="p2409" style="display: contents;"></div>',
+        cbalance='<div id="e54d6616-1969-45fd-85bf-b1bdd2c2447b" data-root-id="p2211" style="display: contents;"></div>',
+        rbalance='<div id="a3859fe3-1e75-45e9-9dd4-b4e42014647e" data-root-id="p2280" style="display: contents;"></div>',
+    )
 
     db.session.add(front_calibration)
     db.session.add(rear_calibration)
@@ -95,6 +132,8 @@ def add_test_data():
     db.session.add(setup)
     db.session.add(track)
     db.session.add(session)
+    db.session.add(session2)
+    db.session.add(session_html)
     db.session.add(board)
     db.session.commit()
 
