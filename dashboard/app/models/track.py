e49c1ffdde3c445aad7b2854558546a156be1dd6
sghctoma <sghctoma@gmail.com>
2024-02-11 11:02:18 +0100

    [api] Add validate method to Track

--- dashboard/app/models/track.py
+++ dashboard/app/models/track.py
@@ -1,3 +1,4 @@
+import json
 import uuid
 
 from dataclasses import dataclass
@@ -10,3 +11,16 @@ from app.models.synchronizable import Synchronizable
 class Track(db.Model, Synchronizable):
     id: uuid.UUID = db.Column(db.Uuid(), primary_key=True, default=uuid.uuid4)
     track: str = db.Column(db.String, nullable=False)
+
+    def validate(self) -> bool:
+        try:
+            track = json.loads(self.track)
+            data = [
+                track['lat'],
+                track['lon'],
+                track['ele'],
+                track['time']
+            ]
+        except BaseException:
+            return False
+        return len(set(map(len, data))) == 1 and len(data[0]) >= 1
