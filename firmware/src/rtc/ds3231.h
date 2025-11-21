c587c5308cb95a0ef215cf4d91923cc15998781e
Ivan Dilchovski <root.darkstar@gmail.com>
2025-11-21 23:26:17 +0200

    Switch from datetime_t to time.h tm in ds3231 & ntp

--- firmware/src/rtc/ds3231.h
+++ firmware/src/rtc/ds3231.h
@@ -1,6 +1,7 @@
 #ifndef _DS3231_H
 #define _DS3231_H
 
+#include <time.h>
 #include "../pio_i2c/pio_i2c.h"
 
 #ifdef __cplusplus
@@ -18,8 +19,8 @@ void ds3231_init(struct ds3231 *d, PIO pio, uint sm,
                  int (*i2c_write)(PIO, uint, uint8_t, const uint8_t *, uint),
                  int (*i2c_read)(PIO, uint, uint8_t, uint8_t *, uint));
 
-void ds3231_get_datetime(struct ds3231 *d, datetime_t *dt);
+void ds3231_get_datetime(struct ds3231 *d, struct tm *tm);
 
-void ds3231_set_datetime(struct ds3231 *d, datetime_t *dt);
+void ds3231_set_datetime(struct ds3231 *d, const struct tm *tm);
 
 #endif // _DS3231_H
