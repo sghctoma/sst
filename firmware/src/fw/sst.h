4cbd51b1d1022cdc38916ad32299d691df5f43a4
Ivan Dilchovski <root.darkstar@gmail.com>
2026-01-04 12:25:18 +0200

    [fw] Add chunks & records for IMUs to the SST file

--- firmware/src/fw/sst.h
+++ firmware/src/fw/sst.h
@@ -24,33 +24,59 @@ enum state {
 };
 #define STATES_COUNT 13
 
-#define CHUNK_TYPE_RATES 0x00
-#define CHUNK_TYPE_TELEMETRY 0x01
-#define CHUNK_TYPE_MARKER 0x02
+#define CHUNK_TYPE_RATES    0x00
+#define CHUNK_TYPE_TRAVEL   0x01
+#define CHUNK_TYPE_MARKER   0x02
+#define CHUNK_TYPE_IMU      0x03
+#define CHUNK_TYPE_IMU_META 0x04
 
-struct chunk_header {
-    uint8_t type;
-    uint16_t length;
-} __attribute__((packed));
-
-struct rate_entry {
-    uint8_t type;
-    uint16_t rate;
-} __attribute__((packed));
-
-struct header {
-    char magic[3];
+// Header for the entire SST file
+struct sst_header {
+    char magic[3]; // Always SST
     uint8_t version;
     uint32_t padding;
     time_t timestamp;
 };
 
-struct record {
+// Header for a single chunk
+struct chunk_header {
+    uint8_t type; // CHUNK_TYPE_*
+    uint16_t length;
+} __attribute__((packed));
+
+// Samplerate for a given telemetry type.
+// By convention there must be only one per type.
+// All samplerate records must immediately follow the SST header.
+struct samplerate_record {
+    uint8_t type;  // CHUNK_TYPE_ TRAVEL/IMU
+    uint16_t rate; // Hz
+} __attribute__((packed));
+
+// Travel telemetry record
+struct travel_record {
     uint16_t fork_angle;
     uint16_t shock_angle;
 };
 
-enum command { OPEN, DUMP, FINISH, MARKER };
+// Meta for the IMU records.
+// As IMU records are in raw device values which depend on hardware and/or configured sensitivity,
+// how many units make 1 G/DPS is described in the meta.
+// By convention there must be only one meta per sensor and they must precede all IMU records.
+struct imu_meta_record {
+    uint8_t location_id; // 0=Frame, 1=Fork, 2=Rear
+    float accel_lsb_per_g;
+    float gyro_lsb_per_dps;
+} __attribute__((packed));
+
+// IMU records do not encode which sensor are they from but they are read from hardware sequentially
+// So it is assumed that the order of IMU records is the same as the order of meta records
+// e.g. frame/fork/frame/fork/frame/fork....
+struct imu_record {
+    int16_t ax, ay, az;
+    int16_t gx, gy, gz;
+};
+
+enum command { OPEN, DUMP_TRAVEL, DUMP_IMU, FINISH, MARKER };
 
 #define BUFFER_SIZE 2048
 #define FILENAME_LENGTH                                                                                                \
